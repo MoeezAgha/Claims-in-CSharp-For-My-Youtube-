b1ae7be9e8f62c774ed38c9ba64798508bdfa0f9
none <moeez_agha@outlook.com>
2021-06-27 11:21:49 -0800

    Add project files.

---
+++ Claims/Pages/Login.cshtml.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Claims.Pages
+{
+    public class LoginModel : PageModel
+    {
+
+        [BindProperty]
+        public Credential Credential { get; set; }
+        //mvvm but it is mvc 
+
+
+        public void OnGet()
+        {
+            this.Credential = new Credential { UserName = "admin" };
+        }
+
+        public async Task <IActionResult> OnPost()
+        {
+            var cookieName = "MrCookiesAuth";
+            if (!ModelState.IsValid) return Page();
+
+            if (Credential.UserName == "admin" && Credential.Password == "agha")
+            {
+               var claims = new List<Claim> {
+               new Claim(ClaimTypes.Name, "admin"),
+               new Claim(ClaimTypes.Email, "admin@moeez.com"),
+               new Claim("Department", "HR")
+
+               };
+              
+                //MustHaveAgha
+
+                var identity = new ClaimsIdentity(claims, cookieName);
+                ClaimsPrincipal claimsPrincipal = new ClaimsPrincipal(identity);
+                await  HttpContext.SignInAsync(cookieName, claimsPrincipal);
+
+                return RedirectToPage("/Index");
+            }
+
+
+
+            return Page();
+
+        }
+
+
+
+
+
+    }
+    
+
+    public class Credential { 
+    
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+
+    }
+}
---
+++ Claims/Startup.cs
@@ -0,0 +1,74 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Claims
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            // First  services.AddAuthentication().AddCookie("MrCookiesAuth",
+            //          options => { options.Cookie.Name = "MrCookiesAuth"; }) ;// add this after error 
+
+            services.AddAuthentication("MrCookiesAuth").AddCookie("MrCookiesAuth",
+             options => { options.Cookie.Name = "MrCookiesAuth";
+                 options.LoginPath = "/Login";
+             });// add this after error 
+
+            services.AddAuthorization(policy =>
+            {
+                policy.AddPolicy("MustHaveAgha",
+                    policy => policy.RequireClaim("Department", "HR"));
+            });
+            services.AddRazorPages();
+      
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+            app.UseAuthentication(); //above step 2
+            app.UseAuthorization();
+       
+
+            
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+            });
+        }
+    }
+}
